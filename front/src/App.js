5b077534efd4226a2e0de0e3e3c860513fed870a
Shlandie <justikas12@gmail.com>
2023-05-30 12:09:30 +0300

    Adding front folder

---
+++ front/src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+import Navigation from "./components/Navigation/Navigation";
+
+function App() {
+  return (
+    <Navigation />
+  );
+}
+
+export default App;
---
+++ front/src/components/Navigation/Navigation.js
@@ -0,0 +1,19 @@
+function Navigation() {
+    return (
+
+        <div className="navigation__main">
+
+            <div className="navigation__order">
+                <a href=""><span>Order</span></a>
+            </div>
+            <a href=""><img className="navigation__logo" alt="Nice Cozy Food" /></a>
+            <div className="navigation__contacts">
+                <a href=""><span>Contact us</span></a>
+            </div>
+        </div>
+
+
+    );
+}
+
+export default Navigation;
