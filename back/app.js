91b7d44b28cf814648acb1ed2b66cc00e8867d81
Shlandie <justikas12@gmail.com>
2023-05-30 14:45:17 +0300

    File structure and estbalishing connection to remote DB

---
+++ back/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(console.log("Connected!"))
+    .catch((err) => console.log(err));
+
+app.listen(5000, () => {
+    console.log("Listening on port 5000");
+})
